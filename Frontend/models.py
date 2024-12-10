0af935d8b8ee0269fc9af1392b937b39bc6f86e3
Bibinku <bibinku01@gmail.com>
2024-12-10 22:11:14 +0530

    Create ContactDB

--- Frontend/models.py
+++ Frontend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+# Creating Models for Contact Us
+class ContactDB(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+    email= models.EmailField(max_length=50,null=True, blank=True)
+    number = models.IntegerField(null=True, blank=True)
